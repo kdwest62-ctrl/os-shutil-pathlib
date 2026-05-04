73906cdae5e392d47a19da45a8c974da36d32342
Kear Ibale <kdwest62@gmail.com>
2026-05-04 14:31:28 +0800

    Refactor file renaming logic and improve user prompts

--- file-renamer.py
+++ file-renamer.py
@@ -1,28 +1,38 @@
 from pathlib import Path
 
-dir_path = Path(input("Directory path: "))
-if dir_path.exists():
-    file_extensions = []
-    items = dir_path.iterdir()
-    for item in items:
+path = Path(input("Directory path: "))
+if path.exists():
+    files = []
+    for item in path.iterdir():
         if item.is_file():
-            file_extensions.append(item.suffix)
-    print(set(file_extensions))
+            files.append(item)
 
-    file_extension = input("File extension to rename: ")
-    if file_extension in file_extensions:
-        num = 1
-        renamed = 0
-        items = dir_path.iterdir()
-        for item in items:
-            if item.is_file():
-                if item.suffix == file_extension:
-                    new_name = item.parent / f'test{num}{file_extension}'
-                    item.rename(new_name)
-                    num += 1
-                    renamed += 1
-        print(f"{renamed} files renamed")
+    if len(files) > 0:
+        extensions = []
+        for file in files:
+            extensions.append(file.suffix)
+
+        print(set(extensions))
+        extension = input("Select files to rename (input extension): ")
+        if extension in extensions:
+            data = []
+            for item in files:
+                data.append(item.stat().st_mtime)
+
+            reference = dict(zip(data, files))
+            sorted_ref = dict(sorted(reference.items()))
+            print("Format: new_name(counter)")
+            new_name = input("new_name: ")
+            count = 1
+            for key in sorted_ref.keys():
+                if sorted_ref[key].suffix == extension:
+                    name_plus_num = new_name + str(count) + extension
+                    sorted_ref[key].rename(path / name_plus_num)
+                    print(f"{sorted_ref[key].name} renamed to {new_name}{count}")
+                    count += 1
+        else:
+            print("Extension not found")
     else:
-        print("File extension not available")
+        print("No files in directory")
 else:
-    print("Path not found")
+    print("Path does not exist")
