94df049c5ccfc0a6ff90d22a8b37ff129d045aa9
Kear Ibale <kdwest62@gmail.com>
2026-05-09 11:25:26 +0800

    Refactor file backup script for improved structure

--- file-backup.py
+++ file-backup.py
@@ -1,87 +1,75 @@
-import shutil
 from pathlib import Path
+import shutil
 
-try:
-    src_path = Path(input("Source path: "))
-    if src_path.exists():
-        file_extensions = []
-        src_path_items = src_path.iterdir()
-        for item in src_path_items:
-            if item.is_file():
-                file_extensions.append(item.suffix)
-        print(set(file_extensions))
+path = Path(input("Directory path: "))
+if path.exists():
+    files = []
+    for item in path.rglob('*'):
+        if item.is_file():
+            files.append(item)
 
-        extensions = []
-        total_ext = int(input("Number of file extensions to backup: "))
-        if total_ext > 0:
-            ext_count = 1
-            while ext_count <= total_ext:
-                extension = input(f"File extension {ext_count} name: ")
-                if extension in file_extensions:
-                    extensions.append(extension)
-                    ext_count += 1
+    if len(files) > 0:
+        backup_list = []
+        print("1. Show backup list\n2. Add files\n3. Remove files\n4. Backup\n5. Exit")
+        while True:
+            choice = input("Select task (num): ")
+            if choice == '1':
+                if len(backup_list) == 0:
+                    print("No files in backup list")
                 else:
-                    print("File extension not found. Try again")
-
-            dst_path = Path(input("Destination path: "))
-            if dst_path.exists():
-                dirs_list = []
-                total_dirs = int(input("Number of directories to create: "))
-                if total_dirs > 0:
-                    dir_count = 1
-                    while dir_count <= total_dirs:
-                        dir_name= input(f"Directory {dir_count} name: ")
-                        new_dir_path = dst_path / dir_name
-                        new_dir_path.mkdir()
-                        dirs_list.append(dir_name)
-                        dir_count += 1
-
-                    destinations = []
-                    print(dirs_list)
-                    for item in extensions:
-                        destination = input(f"Directory for {item}: ")
-                        if destination in dirs_list:
-                            destinations.append(destination)
-                        else:
-                            print("Directory not available")
-                    ext_with_dst = dict(zip(extensions, destinations))
-
-                    if len(ext_with_dst.values()) != 0:
-                        src_path_items = src_path.iterdir()
-                        for item in src_path_items:
-                            if item.is_file():
-                                if item.suffix in ext_with_dst.keys():
-                                    shutil.copy2(str(item), str(dst_path / ext_with_dst[item.suffix]))
-
-                        dirs_files = []
-                        dirs_path_list = []
-                        for item in dirs_list:
-                            dir_path = dst_path / item
-                            for file in dir_path.iterdir():
-                                dirs_path_list.append(file)
-                            count = len(dirs_path_list)
-                            dirs_files.append(count)
-                        files_count = dict(zip(dirs_list, dirs_files))
-                        for key, value in files_count.items():
-                            print(f"Files copied to {key}: {value}")
-
-                        compress = input("Compress directory? (y/n): ")
-                        if compress == 'y':
-                            print(dirs_list)
-                            name = input("Directory name: ")
-                            if name in dirs_list:
-                                archive_name = input("Archive name: ")
-                                archive_dst = dst_path / archive_name
-                                archive_src = dst_path / name
-                                shutil.make_archive(archive_dst, 'zip', archive_src)
-                                print(f"Success! Directory {name} compressed")
-                            else:
-                                print("Directory not found")
+                    for item in backup_list:
+                        print(item)
+            elif choice == '2':
+                nums = [i for i in range(len(files))]
+                reference = dict(zip(nums, files))
+                for k, v in reference.items():
+                    print(k, v)
+                total = int(input("How many files to add? "))
+                count = 0
+                while count < total:
+                    number = int(input("Select file (input number): "))
+                    backup_list.append(reference[number])
+                    print(f"{reference[number]} added to list")
+                    count += 1
+            elif choice == '3':
+                if len(backup_list) == 0:
+                    print("No files in backup list")
+                else:
+                    print(backup_list)
+                    total = int(input("How many files to remove? "))
+                    nums = [i for i in range(len(backup_list))]
+                    reference = dict(zip(nums, backup_list))
+                    for k, v in reference.items():
+                        print(k, v)
+                    count = 0
+                    while count < total:
+                        number = int(input("Select file (num): "))
+                        del backup_list[number]
+                        print(f"{reference[number]} removed from list")
+                        count += 1
+            elif choice == '4':
+                decide = input("Create new directory or use existing (c/u): ")
+                if decide == 'c':
+                    location = Path(input("Location: "))
+                    name = input("Name: ")
+                    dir_path = location / name
+                    dir_path.mkdir()
+                    for item in backup_list:
+                        shutil.copy2(str(item), str(dir_path))
+                        print(f"{item} copied to {dir_path}")
+                elif decide == 'u':
+                    dir_path = Path(input("Directory path: "))
+                    if dir_path.exists():
+                        for item in backup_list:
+                            shutil.copy2(str(item), str(dir_path))
+                            print(f"{item} copied to {dir_path}")
+                    else:
+                        print("Path does not exist")
+            elif choice == '5':
+                break
             else:
-                print("Path not found")
+                print("Option not available")
     else:
-        print("Path not found")
-except FileExistsError:
-    print("Directory already exists")
-except ValueError:
-    print("Please input a number")
+        print("No files in directory")
+else:
+    print("Path does not exist")
