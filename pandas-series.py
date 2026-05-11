00ac80811da027ce8a45cd4d7812c56332a516c3
Kear Ibale <kdwest62@gmail.com>
2026-05-11 08:39:14 +0800

    Delete pandas-series.py

--- pandas-series.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-p1 = input("First: ")
-p2 = input("Second: ")
-p3 = input("Third: ")
-drivers = [p1, p2, p3]
-podium = pd.Series(drivers, index=['P1', 'P2', 'P3'])
-print(podium)
