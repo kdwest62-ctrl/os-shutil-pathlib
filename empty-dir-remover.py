21158532b605821c41615832178db3cc02d3c2bb
Kear Ibale <kdwest62@gmail.com>
2026-05-12 14:59:42 +0800

    Add empty directory remover script
    This script searches for and removes empty directories in a specified path. It allows users to choose between removing all empty directories or selecting specific ones.

---
+++ empty-dir-remover.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+path = Path(input("Search for empty directories (input path): "))
+if path.exists():
+    dirs = []
+    for item in path.rglob('*'):
+        if item.is_dir():
+            dirs.append(item)
+
+    if len(dirs) > 0:
+        empty_dirs = []
+        for item in dirs:
+            count = 0
+            for i in item.iterdir():
+                if i.is_file() or i.is_dir():
+                    count += 1
+            if count == 0:
+                empty_dirs.append(item)
+        if len(empty_dirs) > 0:
+            for item in empty_dirs:
+                print(item)
+            remove = input("Remove all empty directories or selection? (a/s): ")
+            if remove == 'a':
+                for item in empty_dirs:
+                    item.rmdir()
+                    print(f"{item} removed successfully")
+            elif remove == 's':
+                nums = [n for n in range(len(empty_dirs))]
+                reference = dict(zip(nums, empty_dirs))
+                for num, empty_dir in reference.items():
+                    print(num, empty_dir)
+                total = int(input("How many empty directories to remove: "))
+                removed = 0
+                while removed < total:
+                    num = int(input("Select directory (number): "))
+                    reference[num].rmdir()
+                    removed += 1
+                    print(f"{reference[num]} removed successfully")
+        else:
+            print("No empty directories")
+    else:
+        print("No directories found in path")
+else:
+    print("Path does not exist")
